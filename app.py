7878723eaa2536f9785028f360ae48fa5f15656a
microburg <s-omar.hamed@zewailcity.edu.eg>
2024-10-17 14:28:41 +0300

    Created authentication key

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from routes.myrandomstuff_routes import myrandomstuff_bp
+from middleware.auth_middleware import auth_middleware
 from middleware.example_middleware import log_request_middleware
 
 app = Flask(__name__)
 
 # Register middleware
 log_request_middleware(app)
+auth_middleware(app)  # Add this line to enable authentication
 
 # Register blueprint for routes
 app.register_blueprint(myrandomstuff_bp)
